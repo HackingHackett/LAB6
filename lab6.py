5a3af6262a3cc0bda7dec77cb0541285dcce1fe3
hack <hack>
2020-12-18 02:43:17 -0800

    pseduo code

--- lab6.py
+++ lab6.py
@@ -1,16 +1,18 @@
 
+"""num = generate random number
 
+if num is greater than 15,
+  then the result will be "Cherries"
+otherwise if the number is > 10, 
+  then the result wil be "Orange"
+otherwise if the number is  >5, 
+  then the result will be "Plum"
+otherwise if the number is >2, 
+  then the result will be "Melon"
+otherwise if the number is >1,
+  then the result will be "Bell"
+  otherwise
+   the result will be "Bar"
 
-"""
-To start we will generate a random integer between 1 and 20, and 
-based on the result of the random number, we checl to see if it falls under certain range
-if the number is greater than 15, then the result will be "Cherries"
-otherwise if the number is > 10, the the result wil be "Orange"
-otherwise if the number is  >5,  then the result will be "Plum"
-otherwise if the number is >2, then the result will be "Melon"
-otherwise if the number is >1, then the result will be "Bell"
-if the number is not any of the above then the result will be "Bar"
-
-we iterate over using a loop three times and print the results to the user. As an examole "Plum Cherries
-
-"""
+loop three times
+ print the output (fruit) to the user"""
